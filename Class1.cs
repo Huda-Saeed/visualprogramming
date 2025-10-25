1f0e31cb5d428e810204c215a5dfe62b20eeb4ff
Huda-Saeed <hudasaeed190@gmail.com>
2025-10-25 17:53:24 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿namespace ToDoLibrary
+{
+    public class TaskItem
+    {
+        private int TaskId;
+        private string Title;
+        private string Description;
+        private DateTime DueDate;
+        private bool iscompleted;
+        public int taskid
+        {
+            get { return TaskId; }
+            set { TaskId = value; }
+        }
+        public string title
+        {
+            get { return Title; }
+            set { Title = value; }
+        }
+        public string description
+        {
+            get { return Description; }
+            set { Description = value; }
+        }
+        public DateTime duedate
+        {
+            get { return DueDate; }
+            set { DueDate = value; }
+        }
+        public bool iscomplete
+        {
+            get { return iscompleted; }
+            set { iscompleted = value; }
+        }
+        public TaskItem(int id,string tit,string des,DateTime dued)
+        {
+            TaskId = id;    
+            Title = tit;
+            Description = des;
+            DueDate = dued;
+            iscompleted = false;
+            }
+    }
+
+}
---
+++ TaskManager.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoLibrary
+{
+    public class TaskManager
+    {
+        private List<TaskItem> tasksList = new List<TaskItem>();
+        public void AddTask(TaskItem task)
+        { 
+                tasksList.Add(task);
+        }
+        public void UpdateTask(int id,TaskItem updatedTask)
+        {
+            foreach (TaskItem t in tasksList)
+            {
+                if (t.taskid == id)
+                {
+                    t.title = updatedTask.title;
+                    t.description = updatedTask.description;
+                    t.duedate = updatedTask.duedate;
+                    t.iscomplete = updatedTask.iscomplete;
+                }
+            }
+        }
+        public void DeleteTask(int id)
+        {
+            TaskItem taskremove = null;
+            foreach (TaskItem t in tasksList)
+            {
+                if (t.taskid == id)
+                {
+                    taskremove = t;
+                    break;
+                }
+            }
+
+            if (taskremove != null)
+            {
+                tasksList.Remove(taskremove);
+            }
+        }
+        public TaskItem[] GetAlltasks()
+        {
+            return tasksList.ToArray();
+        }
+            public void MarkAsCompleted(int id)
+        {
+            foreach (TaskItem t in tasksList)
+            {
+                if (t.taskid == id)
+                {
+                    t.iscomplete = true;
+                    break;
+                }
+            }
+        }
+    }
+}
